1460c55867c31b9b9830a6c3f915879d5601c6c4
pranaykatariya <pranaykatariya1@gmail.com>
2020-04-02 23:29:37 +0530

    modified

--- RestSpringPSP/src/main/java/com/example/psp/communication/CommunicationController.java
+++ RestSpringPSP/src/main/java/com/example/psp/communication/CommunicationController.java
@@ -1,45 +1,45 @@
-package com.example.psp.communication;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.psp.model.Communication;
-
-@RestController
-public class CommunicationController 
-{
-	@Autowired
-	CommunicationService CommunicationService;
-	
-	@GetMapping("getCommunicationQuestions")
-	public List<Communication> getQuestions()
-	{
-		return CommunicationService.getQuestions();	
-	}
-	
-	
-	@GetMapping("getAllCommunicationQuestions")
-	public List<Communication> getAllQuestions()
-	{
-		return CommunicationService.getAllQuestions();
-	}
-	
-	
-	@PostMapping("storeCommunicationQuestions")
-	public void storeQuestions(@RequestBody List<Communication> list)
-	{
-		CommunicationService.storeQuestions(list);
-	}
-	
-	@GetMapping("deleteCommunicationQuestions")
-	public void deleteQuestions()
-	{
-		CommunicationService.deleteQuestions();
-	}
-	
-}
+//package com.example.psp.communication;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.example.psp.model.Communication;
+//
+//@RestController
+//public class CommunicationController 
+//{
+//	@Autowired
+//	CommunicationService CommunicationService;
+//	
+//	@GetMapping("getCommunicationQuestions")
+//	public List<Communication> getQuestions()
+//	{
+//		return CommunicationService.getQuestions();	
+//	}
+//	
+//	
+//	@GetMapping("getAllCommunicationQuestions")
+//	public List<Communication> getAllQuestions()
+//	{
+//		return CommunicationService.getAllQuestions();
+//	}
+//	
+//	
+//	@PostMapping("storeCommunicationQuestions")
+//	public void storeQuestions(@RequestBody List<Communication> list)
+//	{
+//		CommunicationService.storeQuestions(list);
+//	}
+//	
+//	@GetMapping("deleteCommunicationQuestions")
+//	public void deleteQuestions()
+//	{
+//		CommunicationService.deleteQuestions();
+//	}
+//	
+//}
--- RestSpringPSP/src/main/java/com/example/psp/communication/CommunicationRepository.java
+++ RestSpringPSP/src/main/java/com/example/psp/communication/CommunicationRepository.java
@@ -1,10 +1,10 @@
-package com.example.psp.communication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-import com.example.psp.model.Communication;
-
-public interface CommunicationRepository extends JpaRepository<Communication, Integer> {
-
-}
+//package com.example.psp.communication;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//
+//import com.example.psp.model.Communication;
+//
+//public interface CommunicationRepository extends JpaRepository<Communication, Integer> {
+//
+//}
