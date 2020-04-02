5e3ed149675bd08e664b2a457ccfa1658c88a97b
pranaykatariya <pranaykatariya1@gmail.com>
2020-04-03 00:01:03 +0530

    issue solved, controllers modified

--- RestSpringPSP/src/main/java/com/example/psp/communication/CommunicationController.java
+++ RestSpringPSP/src/main/java/com/example/psp/communication/CommunicationController.java
@@ -32,7 +32,7 @@ public class CommunicationController
 	
 	@PostMapping("storeCommunicationQuestions")
 	public void storeQuestions(@RequestBody List<Communication> list)
-	{
+	{ 
 		CommunicationService.storeQuestions(list);
 	}
 	
